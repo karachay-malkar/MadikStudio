9c2ddb3ba61b5b692f3bfe9b337a8f2bd0206218
taulu <taulantau5@gmail.com>
2026-07-25 15:59:34 +0300

    chore: target MadikStudio GitHub account

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://karachay-malkar.github.io',
+  site: 'https://madikstudio.github.io',
   base: '/MadikStudio',
   output: 'static',
   trailingSlash: 'always',
